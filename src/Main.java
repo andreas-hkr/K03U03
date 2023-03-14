00127b7d0899a9c0bac711d3d5937f2875877bbb
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 14:34:22 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Läs in bränsleförbrukningen i miles/gallon
@@ -5,6 +7,14 @@ public class Main {
         // 1 mile = 1,609 km
         // 1 gallon = 3,785 liter
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ange miles/gallon: ");
+        double milesPerGallon = input.nextDouble();
 
+        double kmPerGallon = milesPerGallon * 1.609;
+        double kmPerLiter = kmPerGallon / 3.785;
+        double literPerMil = 10 / kmPerLiter;
+
+        System.out.println("Det skulle motsvara " + literPerMil + " liter/mil");
     }
 }
