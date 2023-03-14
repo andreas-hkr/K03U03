95251a68ff8763615006f043249d41883d326892
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 14:11:42 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in bränsleförbrukningen i miles/gallon
+        // Översätt till liter/mil och skriv ut
+        // 1 mile = 1,609 km
+        // 1 gallon = 3,785 liter
+
+
+    }
+}
